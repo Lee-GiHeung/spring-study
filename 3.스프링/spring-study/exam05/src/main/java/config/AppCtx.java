1153e2196040339d1853bc8755f850853b07d2db
Lee-GiHeung <dlrlgmd25@gamil.com>
2023-12-08 12:09:19 +0900

    AOP(proxy)

--- 3.스프링/spring-study/exam05/src/main/java/config/AppCtx.java
+++ 3.스프링/spring-study/exam05/src/main/java/config/AppCtx.java
@@ -3,13 +3,26 @@ package config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import aopex.*;
+import config.*;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
+@EnableAspectJAutoProxy(proxyTargetClass = true) // 하위클래스 기반의 프록시
 public class AppCtx {
 
     @Bean
     public Calculator calculator() {
         return new RecCalculator();
+    }
+
+    @Bean
+    public ProxyCache proxyCache() {
+        return new ProxyCache();
+    }
+
+    @Bean
+    public ProxyCalculator proxyCalculator() {
+        return new ProxyCalculator();
     }
 
 }
