a5d5602dc432db06a613198aa5d23d658bd2497a
Lee-GiHeung <dlrlgmd25@gamil.com>
2023-12-11 17:48:47 +0900

    지네릭스 예제

--- 4. 자바보강/generics/src/exam02/Box.java
+++ 4. 자바보강/generics/src/exam02/Box.java
@@ -5,9 +5,8 @@ import exam01.Apple;
 public class Box<T extends Fruit> {
     private T item;
 
-    private static T items;
-    private T[] item3 = new T[];
-
+    //private static T items;
+    //private T[] item3 = new T[];
 
     public T getItem() {
         return item;
@@ -21,6 +20,9 @@ public class Box<T extends Fruit> {
         // T -> Object, 객체를 만드는 시점 -> 투입된 타입으로 형변환 발생
         // T -> Fruit, 객체를 만드는 시점 -> 투입된 타입으로 형변환
         item.print();
+    }
 
+    public <T> void testMethod(T t) { // 지네릭 메서드 - 메서드가 호출될 때 타입이 결정
+        
     }
 }
