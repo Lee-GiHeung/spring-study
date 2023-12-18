5001e9e95db91d33c550c4e0ae2936bb977e4aa7
Lee-GiHeung <dlrlgmd25@gamil.com>
2023-12-18 15:01:06 +0900

    스프링 MVC 쿠키

---
+++ 3.스프링/spring-study/exam07/src/main/java/controllers/TestController.java
@@ -0,0 +1,35 @@
+package controllers;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+    @GetMapping("/cookie/test1")
+    public String test1(HttpServletResponse response, HttpServletRequest request) {
+        Cookie cookie = new Cookie("key1", "value1");
+        response.addCookie(cookie);
+
+        Cookie cookie2 = new Cookie("key2", "value2");
+        cookie2.setPath(request.getContextPath()); // /exam07
+        cookie2.setMaxAge(60 * 60 * 24 * 7);
+        // cookie2.setMaxAge(0); // 1970.1.1 자정 - 쿠키 삭제
+        cookie2.setHttpOnly(true); // false - 자바스크립트로 브라우저에서 조회 가능
+                                // true - 자바스크립트 조회X, 서버쪽에서 요청을 통한 조회 가능
+
+        response.addCookie(cookie2);
+
+        return "test";
+    }
+
+    @GetMapping("/cookie1/test2")
+    public String test2() {
+
+        return "test";
+    }
+    
+}
