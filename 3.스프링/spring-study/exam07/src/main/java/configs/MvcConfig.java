afc5130d8231df9d6742127579d33bb6a50eff99
Lee-GiHeung <dlrlgmd25@gamil.com>
2023-12-20 14:12:30 +0900

    SPRING 프로필

--- 3.스프링/spring-study/exam07/src/main/java/configs/MvcConfig.java
+++ 3.스프링/spring-study/exam07/src/main/java/configs/MvcConfig.java
@@ -9,7 +9,9 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.validation.Validator;
 import org.springframework.web.servlet.config.annotation.*;
 import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
@@ -19,7 +21,7 @@ import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 
 @Configuration
 @EnableWebMvc
-@Import(DbConfig.class)
+@Import(DbConfig2.class)
 public class MvcConfig implements WebMvcConfigurer {
 
     @Autowired
@@ -129,4 +131,13 @@ public class MvcConfig implements WebMvcConfigurer {
         return new Utils();
     }
 
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer configurer() {
+        PropertySourcesPlaceholderConfigurer conf = new PropertySourcesPlaceholderConfigurer();
+        conf.setLocations(
+                new ClassPathResource("application.properties")
+        );
+
+        return conf;
+    }
 }
