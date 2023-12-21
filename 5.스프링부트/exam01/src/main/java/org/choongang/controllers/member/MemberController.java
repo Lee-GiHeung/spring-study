5862fb47e0bdecc89d92ece5a4e05c4db484106b
Lee-GiHeung <dlrlgmd25@gamil.com>
2023-12-21 16:02:05 +0900

    Spring Boot JSON 형식 데이터 출력, 회원목록 출력, 메세지 출력,

--- 5.스프링부트/exam01/src/main/java/org/choongang/controllers/member/MemberController.java
+++ 5.스프링부트/exam01/src/main/java/org/choongang/controllers/member/MemberController.java
@@ -1,9 +1,13 @@
 package org.choongang.controllers.member;
 
 import lombok.extern.slf4j.Slf4j;
+import org.choongang.entities.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDateTime;
 
 @Slf4j
 @Controller
@@ -16,4 +20,19 @@ public class MemberController {
         return "member/join";
     }
 
+    @ResponseBody
+    @GetMapping("/info")
+    public Member info() {
+        Member member = Member.builder()
+                .userNo(1L)
+                .userId("user01")
+                .userPw("12345678")
+                .userNm("사용자")
+                .email("user01@test.org")
+                .regDt(LocalDateTime.now())
+                .modDt(LocalDateTime.now())
+                .build();
+
+        return member;
+    }
 }
--- 5.스프링부트/exam01/src/main/java/org/choongang/entities/Member.java
+++ 5.스프링부트/exam01/src/main/java/org/choongang/entities/Member.java
@@ -1,5 +1,7 @@
 package org.choongang.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,9 +17,14 @@ public class Member {
     @Id // Primary Key : 기본키
     private Long userNo;
     private String userId;
+    @JsonIgnore
     private String userPw;
     private String userNm;
     private String email;
+
+    @JsonFormat(pattern = "yyyy.MM.dd HH:mm")
     private LocalDateTime regDt;
+
+    @JsonFormat(pattern = "yyyy.MM.dd HH:mm")
     private LocalDateTime modDt;
 }
