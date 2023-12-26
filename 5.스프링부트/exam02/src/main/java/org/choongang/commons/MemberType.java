4f93cdfe060280cf8a15544af078d5f4b569f5ff
Lee-GiHeung <dlrlgmd25@gamil.com>
2023-12-26 12:40:19 +0900

    Spring Boot JPA 엔티티 생명주기 테스트 코드, @Table로 테이블 생성, 엔티티 매핑 관련 애노테이션 사용

---
+++ 5.스프링부트/exam02/src/main/java/org/choongang/commons/MemberType.java
@@ -0,0 +1,8 @@
+package org.choongang.commons;
+
+public enum MemberType {
+    MANAGER,
+    ADMIN, // 관리자
+    USER // 일반 회원
+
+}
--- 5.스프링부트/exam02/src/main/java/org/choongang/entities/Member.java
+++ 5.스프링부트/exam02/src/main/java/org/choongang/entities/Member.java
@@ -1,20 +1,55 @@
 package org.choongang.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
+import org.choongang.commons.MemberType;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.sql.Date;
 import java.time.LocalDateTime;
-
 
 @Data
 @Entity
-public class Member {
-    @Id
+@Table(name="USERS",
+        indexes = {@Index(name="idx_member_createdAt", columnList = "createdAt DESC")})
+//@EntityListeners(AuditingEntityListener.class)
+public class Member extends Base {
+    @Id @GeneratedValue
     private Long seq;
+
+    @Column(length = 80, unique = true, nullable = false)
     private String email;
+
+    @Column(length = 40, nullable = false)
     private String name;
-    private String password;
+
+    @Column(length = 65, name="userPw", nullable = false)
+    private String password; // varchar2
+
+    //@Lob
+    @Transient
+    private String introduction; // CLOB
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 10)
+    private MemberType type;
+
+    /*
+    //@CreationTimestamp // INSERT SQL 실행시
+    @CreatedDate
     private LocalDateTime createdAt;
+
+    //@UpdateTimestamp // UPDATE SQL 실행시
+    @LastModifiedDate
     private LocalDateTime modifiedAt;
+    */
+
+    //@Temporal(TemporalType.DATE) // 날짜
+    //@Temporal(TemporalType.TIME) // 시간
+    //@Temporal(TemporalType.TIMESTAMP) // 날짜 + 시간
+    //public Date dt;
 }
