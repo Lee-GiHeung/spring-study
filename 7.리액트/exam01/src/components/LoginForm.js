596327df1d4d8fd7db3bf47d81f69f2cab6314f5
Lee-GiHeung <dlrlgmd25@gmail.com>
2024-02-07 16:27:19 +0900

    loginForm

---
+++ 7.리액트/exam01/src/components/LoginForm.js
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+
+const LoginForm = () => {
+  const [form, setForm] = useState({
+    userId: '',
+    userPw: '',
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault(); // 기본 기능을 차단
+  };
+
+  const { userId, userPw } = form;
+
+  const onChange = (e) => {
+    console.log(e.currentTarget.name, e.currentTarget.value);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <dl>
+        <dt>아이디</dt>
+        <dd>
+          <input type="text" name="userId" onChange={onChange} value={userId} />
+        </dd>
+      </dl>
+      <dl>
+        <dt>비밀번호</dt>
+        <dd>
+          <input
+            type="password"
+            name="userPw"
+            onChange={onChange}
+            value={userPw}
+          />
+        </dd>
+      </dl>
+      <button type="submit">로그인</button>
+    </form>
+  );
+};
+
+export default LoginForm;
